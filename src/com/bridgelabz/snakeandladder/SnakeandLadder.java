5a346cfa846bd5105c6e11abb9e47fdf88ad05f9
kdjay517 <kdjay517@gmail.com>
2021-11-07 16:54:27 +0530

    added snake and ladder user case3

---
+++ src/com/bridgelabz/snakeandladder/SnakeandLadder.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.snakeandladder;
+
+import java.util.*;
+
+public class SnakeandLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Enter press 1 to start the Snake and Ladder game:");
+		Scanner sc = new Scanner(System.in);
+		int start = sc.nextInt();
+		if (start == 1) {
+			System.out.println("________________ Game is going to Start _________________");
+			System.out.println("________________Checking for Player Option _________________");
+			int playerOption = (int) (Math.floor(Math.random() * 10) % 3);
+			int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+			switch (playerOption) {
+				case 0:
+					System.out.println("Player selected for NoPlay at Position Number:" + start);
+					break;
+				case 1:
+					System.out.println("Player got ladder at Position Number:" + start);
+					start += diceNumber;
+					System.out.println("After ladder player is went to position:" + start);
+					break;
+				case 2:
+					System.out.println("Player got snake at position Number:" +start);
+					start -= diceNumber;
+					System.out.println("After snake player went to position Number:" +start);
+					break;
+
+			}
+			
+
+		}
+		else 
+			System.out.println("Press 1 to start the Game");
+	}
+
+}
